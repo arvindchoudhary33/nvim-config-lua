8b076735ff0a8d9de9e022b5b501f038016f614f
amlp33 <sharmarahul965797@gmail.com>
2022-09-17 15:55:20 +0530

    Kanagawa telescope changes

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -1,7 +1,7 @@
-local status, null_ls = pcall(require, "null-ls")
+--[[ local status, null_ls = pcall(require, "null-ls")
 if (not status) then return end
 local formatting = null_ls.builtins.formatting
-local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+local augroup_format = vim.api.nvim_create_augroup("format", { clear = true })
 
 null_ls.setup({
   debug = false,
@@ -10,6 +10,33 @@ null_ls.setup({
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
+    null_ls.builtins.diagnostics.zsh
+  },
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentformattingprovider then
+      vim.api.nvim_clear_autocmds { buffer = 0, group = augroup_format }
+      vim.api.nvim_create_autocmd("bufwritepre", {
+        group = augroup_format,
+        buffer = 0,
+        callback = function() vim.lsp.buf.formatting_seq_sync() end
+      })
+    end
+  end,
+}) ]]
+
+
+
+local status, null_ls = pcall(require, "null-ls")
+if (not status) then return end
+
+local formatting = null_ls.builtins.formatting
+local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+
+null_ls.setup {
+  debug = false,
+  sources = {
+    formatting.prettier,
+    null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.diagnostics.zsh
   },
   on_attach = function(client, bufnr)
@@ -22,4 +49,4 @@ null_ls.setup({
       })
     end
   end,
-})
+}
